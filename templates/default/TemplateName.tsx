300be0ae867f26fdb6c7ad0ed925d27b240f0320
Leonid Lichkun <lionproklon@gmail.com>
2023-01-31 11:32:56 +0300

    Fixed template component

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,10 @@
-import styles from './TemplateName.module.scss';
+import React from "react";
+import styles from "./TemplateName.module.scss";
 
-interface TemplateNameProps { }
+interface ITemplateName {}
 
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
-    TemplateName Component
-  </div>
+export const TemplateName = ({}: ITemplateName) => (
+    <div className={styles.TemplateName} data-testid="TemplateName">
+        TemplateName Component
+    </div>
 );
