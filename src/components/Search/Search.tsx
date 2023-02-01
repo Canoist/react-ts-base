8b976ad918fd815b7eea42942a60694f9fd52621
Leonid Lichkun <lionproklon@gmail.com>
2023-02-01 15:00:08 +0300

    Fill content of search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,36 @@
-import React from "react";
+import { ReactComponent as SearchIcon } from "assets/search-icon.svg";
+import React, { useRef } from "react";
+import { GitHubResError } from "types";
 import styles from "./Search.module.scss";
 
-interface ISearch {}
+interface ISearch {
+    error: GitHubResError;
+    onSubmit: (text: string) => void;
+}
 
-export const Search = ({}: ISearch) => (
-    <div className={styles.search}>Search</div>
-);
+export const Search = ({ error, onSubmit }: ISearch) => {
+    const searchRef = useRef<HTMLFormElement | null>(null);
+
+    const handleSubmit = (event: React.FormEvent) => {};
+
+    return (
+        <form
+            className={styles.search}
+            ref={searchRef}
+            onSubmit={() => handleSubmit}
+            autoComplete="off">
+            <label htmlFor="search" className={styles.label}>
+                <SearchIcon />
+            </label>
+            <input
+                type="text"
+                className={styles.textField}
+                id="search"
+                name="username"
+                placeholder="Search GitHub username..."
+            />
+            {!!error && <div className={styles.error}>{error.message}</div>}
+            Search
+        </form>
+    );
+};
