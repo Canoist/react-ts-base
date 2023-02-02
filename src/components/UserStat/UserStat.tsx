aea02b032eb14cd839c45ba796136b4b368e5e34
Leonid Lichkun <lionproklon@gmail.com>
2023-02-02 23:07:51 +0300

    Created base structure of UserStat component

--- src/components/UserStat/UserStat.tsx
+++ src/components/UserStat/UserStat.tsx
@@ -1,8 +1,23 @@
 import React from "react";
+import { GitHubUser } from "types";
 import styles from "./UserStat.module.scss";
 
-interface IUserStat {}
+interface IUserStat
+    extends Pick<GitHubUser, "following" | "followers" | "public_repos"> {}
 
-export const UserStat = ({}: IUserStat) => (
-    <div className={styles.userStat}>UserStat</div>
+export const UserStat = ({ public_repos, followers, following }: IUserStat) => (
+    <div className={styles.userStat}>
+        <div className={styles.styleInfo}>
+            <div className={styles.infoTitle}>Repos</div>
+            <div className={styles.infoNumber}>{public_repos}</div>
+        </div>
+        <div className={styles.styleInfo}>
+            <div className={styles.infoTitle}>Followers</div>
+            <div className={styles.infoNumber}>{followers}</div>
+        </div>
+        <div className={styles.styleInfo}>
+            <div className={styles.infoTitle}>Following</div>
+            <div className={styles.infoNumber}>{following}</div>
+        </div>
+    </div>
 );
