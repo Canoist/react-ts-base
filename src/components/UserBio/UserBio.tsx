8a3271e1a38e100d4193ae11a339c8388a9db338
Leonid Lichkun <lionproklon@gmail.com>
2023-02-03 21:48:45 +0300

    fixed using an class .empty at UserBio component

--- src/components/UserBio/UserBio.tsx
+++ src/components/UserBio/UserBio.tsx
@@ -5,7 +5,7 @@ import styles from "./UserBio.module.scss";
 interface IUserBio extends Pick<GitHubUser, "bio"> {}
 
 export const UserBio = ({ bio }: IUserBio) => (
-    <p className={styles.userBio + bio ? "" : ` ${styles.empty}`}>
+    <p className={`${styles.userBio} ${bio ? "" : styles.empty}`}>
         {bio || "This profile has no bio"}
     </p>
 );
